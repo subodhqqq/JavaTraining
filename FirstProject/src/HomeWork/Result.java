567d396a4b8e2ed7f286cc681f2fab4c527a24b5
subodh stha <sthasubodh31@gmail.com>
2021-11-28 08:20:46 +0545

    commit update see result

---
+++ FirstProject/src/HomeWork/Result.java
@@ -0,0 +1,12 @@
+package HomeWork;
+
+public class Result {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SeeResult sr=new SeeResult();
+		sr.result();
+
+	}
+
+}
